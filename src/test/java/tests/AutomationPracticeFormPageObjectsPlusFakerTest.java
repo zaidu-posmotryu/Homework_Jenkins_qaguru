d8cfe9dc9cea537a1008110e7d1aa90fc314e3f3
zaidu-posmotryu <cherchimda@gmail.com>
2022-11-19 22:31:07 +0400

    added config remote

--- src/test/java/tests/AutomationPracticeFormPageObjectsPlusFakerTest.java
+++ src/test/java/tests/AutomationPracticeFormPageObjectsPlusFakerTest.java
@@ -1,10 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import pages.AutomationPracticeFormPage;
-import pages.TestUserData;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.AutomationPracticeFormPage;
+import pages.TestUserData;
+
+import static io.qameta.allure.Allure.step;
 
 public class AutomationPracticeFormPageObjectsPlusFakerTest extends TestUserData {
     AutomationPracticeFormPage automationPracticeFormPage = new AutomationPracticeFormPage();
@@ -13,35 +15,39 @@ public class AutomationPracticeFormPageObjectsPlusFakerTest extends TestUserData
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1600x1200";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
     void checkFormTest() {
-        automationPracticeFormPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(email)
-                .setGender(gender)
-                .setUserNumber(phone)
-                .setBirthDate(day, month, year)
-                .setSubjects(subject)
-                .setHobby(hobby)
-                .uploadPicture()
-                .setAddress(currentAddress)
-                .setLocation(location, town)
-                .submitTheForm();
+        step("Заполняю регистрационную форму", () -> {
+            automationPracticeFormPage.openPage()
+                    .setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setUserNumber(phone)
+                    .setBirthDate(day, month, year)
+                    .setSubjects(subject)
+                    .setHobby(hobby)
+                    .uploadPicture()
+                    .setAddress(currentAddress)
+                    .setLocation(location, town)
+                    .submitTheForm();
+        });
 
-        automationPracticeFormPage.checkResultsTableVisible()
-                .checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Student Email", email)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", phone)
-                .checkResult("Date of Birth", day + " " + month + "," + year)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", hobby)
-                .checkResult("Picture", "this_is_my_life.jpg")
-                .checkResult("Address", currentAddress)
-                .checkResult("State and City", location + " " + town);
+        step("Проверяю правильность заполнения данными", () -> {
+            automationPracticeFormPage.checkResultsTableVisible()
+                    .checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", phone)
+                    .checkResult("Date of Birth", day + " " + month + "," + year)
+                    .checkResult("Subjects", subject)
+                    .checkResult("Hobbies", hobby)
+                    .checkResult("Picture", "this_is_my_life.jpg")
+                    .checkResult("Address", currentAddress)
+                    .checkResult("State and City", location + " " + town);
+        });
     }
 }
