decd50225ae7e93b8dab7b4615d07d17805110cf
zaidu-posmotryu <cherchimda@gmail.com>
2022-11-19 23:27:16 +0400

    added attachments

--- src/test/java/tests/AutomationPracticeFormPageObjectsPlusFakerTest.java
+++ src/test/java/tests/AutomationPracticeFormPageObjectsPlusFakerTest.java
@@ -1,10 +1,17 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.AutomationPracticeFormPage;
 import pages.TestUserData;
+
+import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
@@ -13,9 +20,20 @@ public class AutomationPracticeFormPageObjectsPlusFakerTest extends TestUserData
 
     @BeforeAll
     static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "100.0");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1600x1200";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
     @Test
@@ -49,5 +67,14 @@ public class AutomationPracticeFormPageObjectsPlusFakerTest extends TestUserData
                     .checkResult("Address", currentAddress)
                     .checkResult("State and City", location + " " + town);
         });
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Скриншот");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 }
